8253cf0f828433632746c8b7e2990418dc515749
hakimm77 <hakimboum406@gmail.com>
2022-06-08 23:51:04 +0100

    setup discord server bot

---
+++ src/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} is ready to go !`);
+});
+
+client.on("message", (msg) => {
+  console.log(msg.content);
+  if (msg.content === "fuck" || "niga") {
+    msg.reply("don't fucking curse on my server");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
