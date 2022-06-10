a847f8ae25b9d2747a282677ddfa4ad6327f01e6
hakimm77 <hakimboum406@gmail.com>
2022-06-10 20:33:32 +0100

    cleaned code up

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ path: "./.env.development" });
 const { Client, Intents } = require("discord.js");
+const { onMessage } = require("./controllers/onMessage");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
@@ -8,11 +9,6 @@ client.on("ready", () => {
   console.log(`${client.user.tag} is ready to go !`);
 });
 
-client.on("message", (msg) => {
-  console.log(msg.content);
-  if (msg.content === "fuck") {
-    msg.reply("don't fucking curse on my server");
-  }
-});
+client.on("message", onMessage);
 
 client.login(process.env.BOT_TOKEN);
